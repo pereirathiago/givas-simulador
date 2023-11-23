2b0004651df51d4598638f89cce94dcd5ed70841
Thiago Pereira <pereira.tp.thiago@gmail.com>
2023-11-23 14:38:28 -0300

    feat: novas animações de dança

--- givas_simulator/Assets/scripts/Player.cs
+++ givas_simulator/Assets/scripts/Player.cs
@@ -12,7 +12,6 @@ public class Player : MonoBehaviour
     public float totalHealth;
     public float speed;
     public float gravity;
-    public float damage = 20;
 
     //componentes
     private Animator anim;
@@ -22,8 +21,6 @@ public class Player : MonoBehaviour
     Vector3 moveDirection;
 
     private bool isWalking;
-    private bool waitFor;
-    private bool isHitting;
 
     public bool isDead;
 
@@ -32,8 +29,6 @@ public class Player : MonoBehaviour
 
     public float colliderRadius;
 
-    //armazena os inimigos da cena nesta lista
-    public List<Transform> enemyList = new List<Transform>();
 
     // Start é chamado uma vez antes do primeiro frame 
     void Start()
@@ -48,12 +43,10 @@ public class Player : MonoBehaviour
     void Update()
     {
         //se o player não estiver morto...
-        if (!isDead)
+        if (!isDead && !anim.GetBool("dancing-twerk"))
         {
             //metodo que o permite mover
             Move();
-            //metodo que identifica os inputs
-            GetMouseInput();
         }
 
     }
@@ -77,9 +70,6 @@ public class Player : MonoBehaviour
             //verifica se o personagem está movimentando (caso o input seja maior que zero)
             if (direction.magnitude > 0)
             {
-                //se não estiver executando o ataque
-                if (!anim.GetBool("attacking"))
-                {
                     //variavel local que armazena a rotaçao e o angulo de vizualicao da camera
                     float angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
                     //variavel local que armazena a rotacao porem mais suave
@@ -93,16 +83,9 @@ public class Player : MonoBehaviour
 
                     //muda a animação para andando
                     anim.SetInteger("transition", 1);
+                    anim.SetBool("walking", true);
 
                     isWalking = true;
-                }
-                else
-                {
-                    //se estiver atacando, para de andar e fica parado na cena
-                    anim.SetBool("walking", false);
-                    moveDirection = Vector3.zero;
-                }
-
 
             }
             else if (isWalking)
@@ -124,145 +107,5 @@ public class Player : MonoBehaviour
         //move o personagem
         controller.Move(moveDirection * Time.deltaTime);
 
-    }
-
-    /// <summary>
-    /// Detecta se pressionou o botão esquerdo do mouse
-    /// </summary>
-    void GetMouseInput()
-    {
-        if (controller.isGrounded)
-        {
-            if (Input.GetMouseButtonDown(0))
-            {
-                //se pressionar enquanto estiver andando, para de andar e executa imediatamente a animação de ataque
-                if (anim.GetBool("walking"))
-                {
-                    anim.SetBool("walking", false);
-                    anim.SetInteger("transition", 0);
-                }
-
-                //se estiver parado e pressionar o mouse, executa o ataque
-                if (!anim.GetBool("walking"))
-                {
-                    StartCoroutine("Attack");
-                }
-
-
-            }
-        }
-    }
-
-    /// <summary>
-    /// Corrotina de ataque do player
-    /// </summary>
-    IEnumerator Attack()
-    {
-        //se não estiver tomando dano
-        if (!waitFor && !isHitting)
-        {
-            waitFor = true;
-            anim.SetBool("attacking", true);
-            anim.SetInteger("transition", 2);
-
-            //sepera 0.4 segundos
-            yield return new WaitForSeconds(0.4f);
-
-            //pega os inimigos mais próximos
-            GetEnemiesList();
-
-            //executa um laço que percorre cada elemento da lista
-            foreach (Transform e in enemyList)
-            {
-                //armazena os objetos na variavel local "enemy"
-                //CombatEnemy enemy = e.GetComponent<CombatEnemy>();
-
-                //se houver inimigo na lista, o enemy será diferente de nulo
-               /* if (enemy != null)
-                {
-                    //aplica dano no inimigo em questão
-                    enemy.GetHit(damage);
-                }*/
-            }
-
-            //aguarda 1 segundo
-            yield return new WaitForSeconds(1f);
-
-            //edecuta animação idle
-            anim.SetInteger("transition", 0);
-            //para de atacar
-            anim.SetBool("attacking", false);
-            waitFor = false;
-        }
-    }
-
-    /// <summary>
-    /// - Método que captura todos os objetos com colisores próximos ao player
-    /// - Se houver inimigos no meio, armazena na lista "enemyList"
-    /// </summary>
-    void GetEnemiesList()
-    {
-        //limpa a lista antes de preenche-la
-        enemyList.Clear();
-
-        //laço que encontra objetos com colisores em um raio do player
-        foreach (Collider c in Physics.OverlapSphere((transform.position + transform.forward * colliderRadius), colliderRadius))
-        {
-            //se houver inimigo na lista de objetos
-            if (c.gameObject.CompareTag("Enemy"))
-            {
-                //armazena o objeto do inimigo na lista
-                enemyList.Add(c.transform);
-            }
-        }
-    }
-
-
-
-
-
-    /// <summary>
-    /// - Método chamado quando o player toma um hit
-    /// - Executa a animação de hit
-    /// - Executa o game over se a vida for menor que zero
-    /// </summary>
-    /// <param name="damage">retorna valor do dano que o player receberá</param>
-    public void GetHit(float damage)
-    {
-        totalHealth -= damage;
-
-        if (totalHealth > 0)
-        {
-            //player ainda está vivo
-            StopCoroutine("Attack");
-            anim.SetInteger("transition", 3);
-            isHitting = true;
-            StartCoroutine("RecoveryFromHit");
-        }
-        else
-        {
-            //player morre
-            isDead = true;
-            anim.SetTrigger("die");
-        }
-    }
-
-    /// <summary>
-    /// Corrotina aguarda 1 segundo para liberar o player a atacar novamente
-    /// </summary>
-    /// <returns></returns>
-    IEnumerator RecoveryFromHit()
-    {
-        yield return new WaitForSeconds(1f);
-        anim.SetInteger("transition", 0);
-        isHitting = false;
-        anim.SetBool("attacking", false);
-
-    }
-
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position + transform.forward, colliderRadius);
     }
 }
---
+++ givas_simulator/Assets/scripts/PlayerDancing.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDancing : MonoBehaviour
+{
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DancingTwerk();
+        HeadSpinning();
+    }
+
+    void DancingTwerk()
+    {
+        if (!anim.GetBool("walking"))
+        {
+            if(Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                anim.SetTrigger("isDancingTwerk");
+                anim.SetBool("dancing-twerk", true);
+                Invoke("FinishDancingTwerk", 15f);
+            }
+        } else
+        {
+            anim.SetBool("dancing-twerk", false);
+        }
+    }
+
+
+   /* void HeadSpinning()
+    {
+        if (!anim.GetBool("walking"))
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                anim.SetTrigger("isHeadSpinning");
+                new WaitForSeconds(1f);
+                anim.SetTrigger("isHeadSpinning");
+            }
+        }
+    }*/
+
+    private IEnumerator SpinHeadAfterDelay(float delay)
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            anim.SetTrigger("isHeadSpinning");
+            yield return new WaitForSeconds(delay);
+        }
+        anim.SetTrigger("isHeadSpinningEnd");
+    }
+
+    private void HeadSpinning()
+    {
+        if (!anim.GetBool("walking"))
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                StartCoroutine(SpinHeadAfterDelay(0.25f));
+            }
+        }
+    }
+
+    private void FinishDancingTwerk()
+    {
+        anim.SetBool("dancing-twerk", false);
+    }
+}
